e406f638d36d1fdd8447ff99bcfd0b4d1fc74d75
batista0523 <elisaulbatista@pursuit.org>
2025-12-23 15:13:38 -0500

    added

--- DB/db.config.ts
+++ DB/db.config.ts
@@ -1,5 +1,5 @@
-import pgPromise from 'pg-promise';
-import dotenv from 'dotenv';
+import pgPromise from "pg-promise";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -10,7 +10,10 @@ const cn = {
   port: Number(process.env.PG_PORT),
   database: process.env.PG_DATABASE as string,
   user: process.env.PG_USER as string,
-  
+  password: process.env.PG_PASSWORD as string,
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false,
 };
 
 const db = pgp(cn);
